a4b92a68183d21385a53c72faabc6706cae42392
XavierColladoSanchez <xavescu@gmail.com>
2021-10-12 23:58:35 +0200

    Adding Clues constants and implementing Board class (just getters/setters)

--- src/main/java/org/example/model/Board.java
+++ src/main/java/org/example/model/Board.java
@@ -1,4 +1,15 @@
 package org.example.model;
 
+import java.util.List;
+
 public class Board {
+    private List<Row> rows;
+
+    public Row getRow(int row) {
+        return rows.get(row);
+    }
+
+    public void addRow(Row row){
+        this.rows.add(row);
+    }
 }
---
+++ src/main/java/org/example/model/enums/Clues.java
@@ -0,0 +1,7 @@
+package org.example.model.enums;
+
+public class Clues {
+    public static int NO_CLUE = 0;
+    public static int WRONG_POSITION = 1;
+    public static int EXACT_POSITION = 2;
+}
