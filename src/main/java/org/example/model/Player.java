b87a07c1ca5d3b6b55df79f9db1cc57b7548014f
XavierColladoSanchez <xavescu@gmail.com>
2021-10-12 23:08:30 +0200

    Player UnitTest definition

--- src/main/java/org/example/model/Player.java
+++ src/main/java/org/example/model/Player.java
@@ -1,4 +1,27 @@
 package org.example.model;
 
 public class Player {
+    private int role;
+    private int points;
+
+    public Player(int role){
+        this.role = role;
+        this.points = 0;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void changeRole() {
+
+    }
+
+    public void addPoints(int points){
+
+    }
 }
---
+++ src/test/java/org/example/model/PlayerTest.java
@@ -0,0 +1,91 @@
+package org.example.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerTest {
+    private final int CODE_MAKER = 1;
+    private final int CODE_BREAKER = 2;
+
+    /**
+     * We validate the change from CODE_MAKER to CODE_BREAKER
+     * UnitTest
+     */
+    @Test
+    public void givenCodeMakerPlayerWhenChangeRoleThenRoleIsCodeBreaker(){
+        //given
+        Player player = new Player(CODE_MAKER);
+
+        //when
+        player.changeRole();
+
+        //then
+        Assert.assertEquals(CODE_BREAKER, player.getRole());
+    }
+
+    /**
+     * We validate the change from CODE_BREAKER to CODE_MAKER
+     * UnitTest
+     */
+    @Test
+    public void givenCodeBreakerPlayerWhenChangeRoleThenRoleIsCodeMaker(){
+        //given
+        Player player = new Player(CODE_BREAKER);
+
+        //when
+        player.changeRole();
+
+        //then
+        Assert.assertEquals(CODE_MAKER, player.getRole());
+    }
+
+    /**
+     * We validate that we add points correctly
+     * UnitTest
+     */
+    @Test
+    public void givenNewPlayerWhenAdding5PointsThenHave5TotalPoints(){
+        //given
+        Player player = new Player(CODE_BREAKER);
+
+        //when
+        player.addPoints(5);
+
+        //then
+        Assert.assertEquals(5, player.getPoints());
+    }
+
+    /**
+     * We validate that we can add points to an existing points count
+     * UnitTest
+     */
+    @Test
+    public void givenPlayerWith5PointsWhenAdding3PointsThenHave8TotalPoints(){
+        //given
+        Player player = new Player(CODE_BREAKER);
+        player.addPoints(5);
+
+        //when
+        player.addPoints(3);
+
+        //then
+        Assert.assertEquals(8, player.getPoints());
+    }
+
+    /**
+     * We validate that nothing breaks when we have more than 9 points
+     * UnitTest
+     */
+    @Test
+    public void givenPlayerWith8PointsWhenAdding4PointsThenHave12TotalPoints(){
+        //given
+        Player player = new Player(CODE_BREAKER);
+        player.addPoints(8);
+
+        //when
+        player.addPoints(4);
+
+        //then
+        Assert.assertEquals(12, player.getPoints());
+    }
+}
