80948e9cfc741eddd855fe9e92bae50a30def3c3
XavierColladoSanchez <xavescu@gmail.com>
2021-10-18 22:15:24 +0200

    Implementing Row class

--- src/main/java/org/example/model/Row.java
+++ src/main/java/org/example/model/Row.java
@@ -1,4 +1,27 @@
 package org.example.model;
 
+import org.example.model.enums.Clues;
+import org.example.model.enums.Colors;
+
+import java.util.ArrayList;
+
 public class Row {
+    private ArrayList<Colors> code = new ArrayList();
+    private ArrayList<Clues> clues = new ArrayList();
+
+    public ArrayList<Clues> getClues() {
+        return clues;
+    }
+
+    public void setClues(ArrayList<Clues> clues) {
+        this.clues = clues;
+    }
+
+    public ArrayList<Colors> getCode() {
+        return code;
+    }
+
+    public void setCode(ArrayList<Colors> code) {
+        this.code = code;
+    }
 }
