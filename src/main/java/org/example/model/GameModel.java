59aa24d4ab0b46b0e948d7033dd1c2b40b86a372
XavierColladoSanchez <xavescu@gmail.com>
2021-10-18 22:30:43 +0200

    Implementing GameModel class

--- src/main/java/org/example/model/GameModel.java
+++ src/main/java/org/example/model/GameModel.java
@@ -1,4 +1,21 @@
 package org.example.model;
 
+import org.example.model.enums.Roles;
+
 public class GameModel {
+    private Board board = new Board();
+    private Player player1 = new Player(Roles.CODE_BREAKER);
+    private Player player2 = new Player(Roles.CODE_MAKER);
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
 }
